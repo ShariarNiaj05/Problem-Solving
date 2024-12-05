2ea8e12630b1c62a239caaa1ef74f83855a28517
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-05 19:03:03 +0600

    Function Composition

--- JavaScript/Function-Composition.js
+++ JavaScript/Function-Composition.js
@@ -1,1 +1,11 @@
 /* Write three functions: one to square a number, one to double a number, and one to add 5 to a number. Compose these functions to create a new function that squares a number, doubles the result, and then adds 5. */
+
+
+
+
+const makeSquare = (num) => {
+    return num * num
+}
+
+
+console.log(makeSquare(2));
