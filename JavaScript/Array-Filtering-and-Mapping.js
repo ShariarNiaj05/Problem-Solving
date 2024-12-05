d5ae471f6b302db1f3566e562d3c861d52a633f1
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-05 18:46:14 +0600

    Array Filtering and Mapping

--- JavaScript/Array-Filtering-and-Mapping.js
+++ JavaScript/Array-Filtering-and-Mapping.js
@@ -1,5 +1,12 @@
 /* Create an array of objects, each representing a person with properties like name, age, and gender. Write a function to filter out all females and then map the remaining people to an array of names. Print the final result. */
 
+const people = [
+    { name: "Alice", age: 25, gender: "female" },
+    { name: "Bob", age: 30, gender: "male" },
+    { name: "Charlie", age: 22, gender: "male" },
+    { name: "David", age: 28, gender: "male" },
+    { name: "Eve", age: 26, gender: "female" }
+];
 
 const genderFilter = (arr) => {
     let onlyMale = []
@@ -12,13 +19,9 @@ const genderFilter = (arr) => {
     return onlyMale
 }
 
+const nameMap = genderFilter(people)
 
-const people = [
-    { name: "Alice", age: 25, gender: "female" },
-    { name: "Bob", age: 30, gender: "male" },
-    { name: "Charlie", age: 22, gender: "male" },
-    { name: "David", age: 28, gender: "male" },
-    { name: "Eve", age: 26, gender: "female" }
-];
 
-genderFilter(people)
+
+
+genderFilter(nameMap)
