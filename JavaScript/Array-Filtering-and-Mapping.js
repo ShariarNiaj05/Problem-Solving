54792a8d0ae3fb869b7fe87cbd3dbd08a32de65b
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-05 18:29:01 +0600

    Array Filtering and Mapping

---
+++ JavaScript/Array-Filtering-and-Mapping.js
@@ -0,0 +1,1 @@
+/* Create an array of objects, each representing a person with properties like name, age, and gender. Write a function to filter out all females and then map the remaining people to an array of names. Print the final result. */
