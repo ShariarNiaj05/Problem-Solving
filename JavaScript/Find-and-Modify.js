228c8764120c0c7dbfb2b32d15e5ecc25e051ed4
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-05 20:49:33 +0600

    Find-and-Modify

--- JavaScript/Find-and-Modify.js
+++ JavaScript/Find-and-Modify.js
@@ -7,7 +7,12 @@
 const people = [
     { name: "Alice", age: 25, gender: "female" },
     { name: "Bob", age: 30, gender: "male" },
-    { name: "Charlie", age: 22, gender: "non-binary" },
+    { name: "Charlie", age: 22, gender: "male" },
     { name: "David", age: 28, gender: "male" },
     { name: "Eve", age: 26, gender: "female" }
 ];
+
+
+const findName = (name) => {
+
+}
