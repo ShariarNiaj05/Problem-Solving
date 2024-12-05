e31c501b5fe44f41d788448a67509ccaafb9b5d6
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-05 19:18:26 +0600

    Sorting-Objects

---
+++ JavaScript/Sorting-Objects.js
@@ -0,0 +1,1 @@
+/* Create an array of objects representing cars with properties like make, model, and year. Write a function to sort the array of cars by the year of manufacture in ascending order. Print the sorted array. */
