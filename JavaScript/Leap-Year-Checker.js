b7ba8b17260fc5452ae1ca4a147eb65cacfd5be3
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-07 23:30:47 +0600

    leap year

--- JavaScript/Leap-Year-Checker.js
+++ JavaScript/Leap-Year-Checker.js
@@ -1,2 +1,12 @@
 /* Write a function that determines whether a given year is a leap year.
 Example: Happy New Year */
+
+const checkLeapYear = (year) => {
+    if (year % 2 === 0 || year % 4 || 0) {
+        return `${year} is leap Year`
+    }
+    else {
+        return `${year} is not leap year`
+    }
+
+}
