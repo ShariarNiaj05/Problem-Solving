b470170c9351cccaa04feb81d9fd1c157029ba4c
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-07 23:25:58 +0600

    array reduce

--- JavaScript/Array-Reduction.js
+++ JavaScript/Array-Reduction.js
@@ -1,1 +1,7 @@
 /* Create an array of numbers. Write a function that uses the reduce method to calculate the sum of all even numbers in the array.  */
+
+
+const arr = [1, 5, 4, 8, 9, 4]
+const arrReduce = arr.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue
+}, 0)
