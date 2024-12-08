d793540ecaa23893608afe6d67f05d1763ac1f65
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-08 23:23:10 +0600

    Unique-Values

--- JavaScript/Unique-Values.js
+++ JavaScript/Unique-Values.js
@@ -2,3 +2,5 @@
 *** Create an array of numbers with some duplicate values. 
 *** Write a function to filter out the duplicate values and *** return a new array with only unique numbers. 
 ***Print the result.  */
+
+const arrWithDuplicates = [5, 5, 8, 7, 3, 3]
