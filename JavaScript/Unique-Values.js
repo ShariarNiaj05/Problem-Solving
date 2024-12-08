22505b555acb40bdd9d1cf594719bfc6640b77c4
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-08 23:21:55 +0600

    Unique-Values

--- JavaScript/Unique-Values.js
+++ JavaScript/Unique-Values.js
@@ -1,1 +1,4 @@
-/* Create an array of numbers with some duplicate values. Write a function to filter out the duplicate values and return a new array with only unique numbers. Print the result.  */
+/* 
+*** Create an array of numbers with some duplicate values. 
+*** Write a function to filter out the duplicate values and *** return a new array with only unique numbers. 
+***Print the result.  */
