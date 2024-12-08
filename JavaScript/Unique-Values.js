9dadc87b1daea8a45a68190ec918f6e72dd280dd
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-08 23:25:16 +0600

    Unique-Values

--- JavaScript/Unique-Values.js
+++ JavaScript/Unique-Values.js
@@ -7,8 +7,10 @@ const arrWithDuplicates = [5, 5, 8, 7, 3, 3]
 
 const removeDuplicates = (arr) => {
     let uniqueArr = []
+    let uniqueValue = arr[0]
 
     for (i = 0; i < arr.length; arr++) {
-
+        const checkIfUnique = a
     }
+    return uniqueArr
 }
