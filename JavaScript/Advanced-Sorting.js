fb85a0efe316698960132289d18c7c035d852c81
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-09 23:39:56 +0600

    cleanup

--- JavaScript/Advanced-Sorting.js
+++ JavaScript/Advanced-Sorting.js
@@ -1,1 +1,4 @@
-/* Create an array of objects representing students with 'name' and 'grades' properties. Write a function to sort the students by average grade in descending order. */
+/* 
+*** Create an array of objects representing students with 'name' and 'grades' properties. 
+*** Write a function to sort the students by average grade in descending order. 
+*/
