606ed3dde78ad36c0a4523b7af7eb007fb67d3ff
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-10 23:06:08 +0600

    advance sorting

--- JavaScript/Advanced-Sorting.js
+++ JavaScript/Advanced-Sorting.js
@@ -14,5 +14,6 @@ const sortByGrade = (data) => {
     const total = grades.reduce((sum, grade) => sum + grade, 0);
     const avg = total / grades.length;
 
+    const sort = avg.sort((a, b) => calculateAverage(b.grades) - calculateAverage(a.grades));
 
 }
