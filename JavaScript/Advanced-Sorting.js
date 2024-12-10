192584ea213d64c27659e110c9fdcd407ca240ef
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-10 23:01:42 +0600

    advance sorting

--- JavaScript/Advanced-Sorting.js
+++ JavaScript/Advanced-Sorting.js
@@ -3,4 +3,9 @@
 *** Write a function to sort the students by average grade in descending order. 
 */
 
-const uniqueNumbers = [5, 8, 9, 55, 14]
+const students = [
+    { name: "Alice", grades: [85, 90, 92] },
+    { name: "Bob", grades: [78, 82, 88] },
+    { name: "Charlie", grades: [95, 96, 97] },
+    { name: "Diana", grades: [88, 89, 91] }
+];
