0e21c98196539ee673024307b501b74fb96f01df
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-11 23:15:50 +0600

    find max

--- JavaScript/Find-Maximum-Value.js
+++ JavaScript/Find-Maximum-Value.js
@@ -1,3 +1,10 @@
 /* 
 *** Write a function that takes an array of numbers and returns the maximum value. 
 */
+
+const numbers = [10, 58, 61, 54, 85, 3, 24, 48]
+
+const maxValue = (arr) => {
+    let biggestNumber = arr[0]
+
+}
