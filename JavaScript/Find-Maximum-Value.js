2639c4dbbbdb861061b5147195a9def60f1fd24c
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-12 18:30:33 +0600

    biggest number

--- JavaScript/Find-Maximum-Value.js
+++ JavaScript/Find-Maximum-Value.js
@@ -6,5 +6,11 @@ const numbers = [10, 58, 61, 54, 85, 3, 24, 48]
 
 const maxValue = (arr) => {
     let biggestNumber = arr[0]
+    for (i = 0; i < arr.length; i++) {
+        if (biggestNumber < i) {
+            biggestNumber = i
+        }
+    }
+    return biggestNumber
 
 }
